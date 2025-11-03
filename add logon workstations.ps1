24781d44b74b58bdd9c1546f0edc8c767a092e14
tzjensen-wu <tzjensen@willamette.edu>
2025-11-03 08:57:04 -0800

    Add files via upload

---
+++ add logon workstations.ps1
@@ -0,0 +1,4 @@
+﻿##note: replace "username" and domain details
+$Workstations = (Get-ADUser username -Properties LogonWorkstations).LogonWorkstations
+$Workstations += ",username.domain.com"
+Set-ADUser username -LogonWorkstations $Workstations
